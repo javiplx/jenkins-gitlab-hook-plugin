d654ae9d6905799d21bdea8e8d5df3ee3b91817c
Javier Palacios <javiplx@gmail.com>
2014-09-13 01:44:48 +0200

    Meld the abstract RequestDetails into its only incarnation

--- models/values/payload_request_details.rb
+++ models/values/payload_request_details.rb
@@ -1,9 +1,17 @@
-require_relative 'request_details'
+require_relative '../services/flat_keys_hash'
 
 module GitlabWebHook
-  class PayloadRequestDetails < RequestDetails
+  class PayloadRequestDetails
     def initialize(payload)
       @payload = payload || raise(ArgumentError.new("request payload is required"))
+    end
+
+    def valid?
+      repository_url.to_s.strip.empty? ? false : true
+    end
+
+    def repository_uri
+      RepositoryUri.new(repository_url)
     end
 
     def repository_url
@@ -28,9 +36,49 @@ module GitlabWebHook
       payload["ref"].to_s.strip
     end
 
+    def branch
+      ref = full_branch_reference
+      return "" unless ref
+
+      refs = ref.split("/")
+      refs.reject { |ref| ref =~ /\A(ref|head)s?\z/ }.join("/")
+    end
+
+    def safe_branch
+      branch.gsub("/", "_")
+    end
+
     def delete_branch_commit?
       after = payload["after"]
       after ? (after.strip.squeeze == "0") : false
+    end
+
+    def commits
+      commits = get_commits || []
+      raise ArgumentError.new("payload must be an array") unless commits.is_a?(Array)
+      commits
+    end
+
+    def commits_count
+      commits ? commits.size : 0
+    end
+
+    def payload
+      payload = get_payload || {}
+      raise ArgumentError.new("payload must be a hash") unless payload.is_a?(Hash)
+      payload
+    end
+
+    def flat_payload
+      @flat_payload ||= payload.extend(FlatKeysHash).to_flat_keys.tap do |flattened|
+        [
+          :repository_url,
+          :repository_name,
+          :repository_homepage,
+          :full_branch_reference,
+          :branch
+        ].each { |detail| flattened[detail.to_s] = self.send(detail) }
+      end
     end
 
     private
