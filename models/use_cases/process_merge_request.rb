0e27535d7e82096e13915da252de098e8a21ab62
Javier Palacios <javiplx@gmail.com>
2014-12-06 18:29:56 +0100

    Initial skeleton for merge request processing

--- models/use_cases/process_merge_request.rb
+++ models/use_cases/process_merge_request.rb
@@ -1,5 +1,9 @@
+require_relative '../exceptions/bad_request_exception'
 
 module GitlabWebHook
   class ProcessMergeRequest
+    def with(details)
+      raise BadRequestException.new("Handling of merge requests not yet implemented")
+    end
   end
 end
--- models/values/merge_request_details.rb
+++ models/values/merge_request_details.rb
@@ -1,6 +1,19 @@
 require_relative 'request_details'
+require_relative '../exceptions/bad_request_exception'
 
 module GitlabWebHook
   class MergeRequestDetails < RequestDetails
+
+    def initialize(payload)
+      raise(ArgumentError.new("request payload is required")) unless payload
+      @kind = payload['object_kind']
+      @payload = payload['object_attributes']
+      throw_cross_repo_exception unless project_id == target_project_id
+    end
+
+    def valid?
+      @kind == 'merge_request'
+    end
+
   end
 end
