ffcbed687e550102ab7b2332ad52217962363d75
Javier Palacios <javiplx@gmail.com>
2014-12-06 16:15:16 +0100

    Add empty classes for the new objects

---
+++ models/use_cases/process_merge_request.rb
@@ -0,0 +1,5 @@
+
+module GitlabWebHook
+  class ProcessMergeRequest
+  end
+end
---
+++ models/values/merge_request_details.rb
@@ -0,0 +1,6 @@
+require_relative 'request_details'
+
+module GitlabWebHook
+  class MergeRequestDetails < RequestDetails
+  end
+end
