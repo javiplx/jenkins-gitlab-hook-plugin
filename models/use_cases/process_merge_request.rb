cd92bd185dd6077fee6bca7d23eed66468ccd896
Javier Palacios <javiplx@gmail.com>
2014-12-13 20:05:23 +0100

    Outline processing of incoming merge request

--- models/use_cases/process_merge_request.rb
+++ models/use_cases/process_merge_request.rb
@@ -1,9 +1,21 @@
-require_relative '../exceptions/bad_request_exception'
 
 module GitlabWebHook
   class ProcessMergeRequest
     def with(details)
-      raise BadRequestException.new("Handling of merge requests not yet implemented")
+      messages = []
+      if details.merge_status != 'mergeable'
+        messages << "Skipping merge request for #{details.repository_name} with #{details.merge_status} status"
+      else
+        case details.state
+        when 'opened', 'reopened'
+          messages << "Skipping not ready merge request for #{details.repository_name}"
+        when 'closed'
+          messages << "Skipping merge request close message"
+        else
+          messages << "Skipping request : merge request status is '#{details.state}'"
+        end
+      end
+      messages
     end
   end
 end
--- spec/use_cases/process_merge_request_spec.rb
+++ spec/use_cases/process_merge_request_spec.rb
@@ -5,11 +5,50 @@ require 'models/root_action_descriptor'
 module GitlabWebHook
   describe ProcessMergeRequest do
 
+    let (:payload) { JSON.parse(File.read('spec/fixtures/new_merge_request_payload.json')) }
+    let (:details) { MergeRequestDetails.new(payload) }
+
     context 'with cross-repo merge request' do
       let(:details) { double(MergeRequestDetails, source_project_id: '14', target_project_id: '15') }
       it 'raise exception' do
         expect { subject.with(details) }.to raise_exception(BadRequestException)
       end
+    end
+
+    context 'when merge request is unchecked' do
+      it 'skips processing' do
+        messages = subject.with(details)
+        expect(messages[0]).to match('Skipping not ready merge request')
+      end
+    end
+
+    context 'when merge request is mergeable' do
+
+      before :each do
+        expect(details).to receive(:merge_status).and_return( 'mergeable' )
+      end
+
+      context 'and status is opened' do
+        it 'and project already exists' do
+        end
+        it 'and project does not exists' do
+        end
+      end
+
+      context 'and status is closed' do
+        it 'and project already exists' do
+        end
+        it 'and project does not exists' do
+        end
+      end
+
+      context 'and status is reopened' do
+        it 'and project already exists' do
+        end
+        it 'and project does not exists' do
+        end
+      end
+
     end
 
   end
