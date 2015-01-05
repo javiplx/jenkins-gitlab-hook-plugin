11a5041236ef2bedd9d939427995817fac171c2f
Javier Palacios <javiplx@gmail.com>
2015-01-05 21:55:40 +0100

    Fix rspec for the new merge request processing flow

--- spec/use_cases/process_merge_request_spec.rb
+++ spec/use_cases/process_merge_request_spec.rb
@@ -7,7 +7,8 @@ module GitlabWebHook
 
     let (:payload) { JSON.parse(File.read('spec/fixtures/new_merge_request_payload.json')) }
     let (:details) { MergeRequestDetails.new(payload) }
-    let (:get_jenkins_projects) { double(named: []) }
+    let (:jenkins_project) { double(AbstractProject, name: 'diaspora', matches?: false, merge_to?: false) }
+    let (:get_jenkins_projects) { double(GetJenkinsProjects) }
     let (:create_project_for_branch) { double(for_merge: []) }
 
     before :each do
@@ -24,68 +25,69 @@ module GitlabWebHook
 
     context 'when merge request is mergeable' do
 
-      before :each do
-        expect(details).to receive(:merge_status).and_return( 'mergeable' )
-      end
+     before :each do
+       expect(details).to receive(:merge_status).and_return( 'mergeable' )
+       expect(get_jenkins_projects).to receive(:matching_uri).and_return([jenkins_project])
+     end
 
-      context 'and status is opened' do
+     [ 'opened', 'reopened' ].each do |status|
+      context "and status is #{status}" do
+        before :each do
+          expect(details).to receive(:state).and_return( status )
+        end
         it 'and project already exists' do
-          expect(get_jenkins_projects).to receive(:named).and_return([double()])
+          expect(jenkins_project).to receive(:matches?).and_return(true)
+          expect(jenkins_project).to receive(:merge_to?).and_return(true)
           expect(create_project_for_branch).not_to receive(:for_merge)
-          messages = subject.with(details)
-          expect(messages[0]).to match('Already created project for')
+          subject.with(details)
         end
         context 'and project does not exists' do
-          it 'and no destination project exists' do
-            messages = subject.with(details)
-            expect(messages[0]).to match('No project candidate for')
+          let (:merge_project) { double(AbstractProject, name: 'diaspora-mr-branchname') }
+          before :each do
+            expect(jenkins_project).to receive(:matches?).and_return(true)
+          end
+          it 'and no candidate target project exists' do
+            expect(create_project_for_branch).to receive(:for_merge).and_return([])
+            expect(BuildNow).not_to receive(:new)
+            subject.with(details)
           end
           it 'and target branch candidate exists' do
-            expect(create_project_for_branch).to receive(:for_merge).and_return([double(ignore_notify_commit?: true, to_s: 'project')])
-            messages = subject.with(details)
-            expect(messages[0]).to match('Created project for')
+            expect(create_project_for_branch).to receive(:for_merge).and_return([merge_project])
+            expect(BuildNow).to receive(:new).with(merge_project).and_return(double(with:''))
+            subject.with(details)
           end
         end
       end
+     end
 
-      context 'and status is closed' do
+     [ 'closed', 'merged' ].each do |status|
+      context "and status is #{status}" do
         before :each do
-          expect(details).to receive(:state).and_return( 'closed' )
+          expect(details).to receive(:state).and_return( status )
           expect(create_project_for_branch).not_to receive(:for_merge)
         end
         it 'and project already exists' do
-          expect(get_jenkins_projects).to receive(:named).and_return([double()])
-          messages = subject.with(details)
-          expect(messages[0]).to match('Deleting project')
+          expect(jenkins_project).to receive(:matches?).and_return(true)
+          expect(jenkins_project).to receive(:merge_to?).and_return(true)
+          expect(jenkins_project).to receive(:delete)
+          subject.with(details)
+        end
+        it 'and only target_branch matches' do
+          allow(jenkins_project).to receive(:merge_to?).and_return(true)
+          expect(jenkins_project).not_to receive(:delete)
+          subject.with(details)
+        end
+        it 'and only source_branch matches' do
+          expect(jenkins_project).to receive(:matches?).and_return(true)
+          expect(jenkins_project).not_to receive(:delete)
+          subject.with(details)
         end
         it 'and project does not exists' do
-          messages = subject.with(details)
-          expect(messages[0]).to match('No project exists for')
+          expect(jenkins_project).not_to receive(:delete)
+          subject.with(details)
         end
       end
-
-      context 'and status is reopened' do
-        before :each do
-          expect(details).to receive(:state).and_return( 'reopened' )
-        end
-        it 'and project already exists' do
-          expect(get_jenkins_projects).to receive(:named).and_return([double()])
-          expect(create_project_for_branch).not_to receive(:for_merge)
-          messages = subject.with(details)
-          expect(messages[0]).to match('Already created project for')
-        end
-        context 'and project does not exists' do
-          it 'and no destination project exists' do
-            messages = subject.with(details)
-            expect(messages[0]).to match('No project candidate for')
-          end
-          it 'and target branch candidate exists' do
-            expect(create_project_for_branch).to receive(:for_merge).and_return([double(ignore_notify_commit?: true, to_s: 'project')])
-            messages = subject.with(details)
-            expect(messages[0]).to match('Created project for')
-          end
-        end
-      end
+     end
 
     end
 
