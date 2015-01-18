cbe8f3bf06d84e84b748ec1eed0ad9ccb889da24
Javier Palacios <javiplx@gmail.com>
2015-01-18 13:11:37 +0100

    Implement commit status POST operation

--- lib/gitlab/client.rb
+++ lib/gitlab/client.rb
@@ -19,12 +19,29 @@ module Gitlab
       res.first['id']
     end
 
+    def set_status(commit, status, url)
+      url = "#{gitlab_url}/projects/1/repository/commits/#{commit}/status?private_token=#{token}"
+      do_post url, :state => status, :target_url => url
+    end
+
     private
 
     def do_request(url)
 
       uri = URI url
       req = Net::HTTP::Get.new uri.request_uri
+
+      http = Net::HTTP.new uri.host, uri.port
+      res = http.request req
+
+      JSON.parse res.body
+    end
+
+    def do_post(url, data)
+
+      uri = URI url
+      req = Net::HTTP::Post.new uri.request_uri
+      req.set_form_data data
 
       http = Net::HTTP.new uri.host, uri.port
       res = http.request req
