f7ba377f166605d861f223a62646afbd89347666
Javier Palacios <javiplx@gmail.com>
2015-01-18 13:29:48 +0100

    Simplify construction of gitlab query URLs

--- lib/gitlab/client.rb
+++ lib/gitlab/client.rb
@@ -4,8 +4,6 @@ require 'json'
 module Gitlab
   class Client
 
-    attr_accessor :gitlab_url, :token
-
     def initialize(url, token)
       @gitlab_url = url
       @token = token
@@ -13,23 +11,26 @@ module Gitlab
     end
 
     def get_id(repo_name)
-      url = "#{gitlab_url}/projects/search/#{repo_name}?private_token=#{token}"
+      url = "projects/search/#{repo_name}"
       res = do_request url
       raise StandardError.new("No valid match") unless res.length == 1
       res.first['id']
     end
 
     def set_status(commit, status, url)
-      url = "#{gitlab_url}/projects/1/repository/commits/#{commit}/status?private_token=#{token}"
+      url = "projects/1/repository/commits/#{commit}/status"
       do_post url, :state => status, :target_url => url
     end
 
     private
 
+    attr_accessor :gitlab_url, :token
+
     def do_request(url)
 
-      uri = URI url
+      uri = URI "#{gitlab_url}/#{url}"
       req = Net::HTTP::Get.new uri.request_uri
+      req['PRIVATE-TOKEN'] = token
 
       http = Net::HTTP.new uri.host, uri.port
       res = http.request req
@@ -39,8 +40,9 @@ module Gitlab
 
     def do_post(url, data)
 
-      uri = URI url
+      uri = URI "#{gitlab_url}/#{url}"
       req = Net::HTTP::Post.new uri.request_uri
+      req['PRIVATE-TOKEN'] = token
       req.set_form_data data
 
       http = Net::HTTP.new uri.host, uri.port
