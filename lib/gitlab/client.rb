53e110fd03f8769c66cdbae2fcd22c5490cb570e
Javier Palacios <javiplx@gmail.com>
2015-01-18 13:45:09 +0100

    Unify GET & POST requests

--- lib/gitlab/client.rb
+++ lib/gitlab/client.rb
@@ -19,31 +19,25 @@ module Gitlab
 
     def set_status(commit, status, ci_url)
       url = "projects/1/repository/commits/#{commit}/status"
-      do_post url, :state => status, :target_url => ci_url
+      do_request url, :state => status, :target_url => ci_url
     end
 
     private
 
     attr_accessor :gitlab_url, :token
 
-    def do_request(url)
+    def do_request(url, data=nil)
 
       uri = URI "#{gitlab_url}/#{url}"
-      req = Net::HTTP::Get.new uri.request_uri
+
+      if data
+        req = Net::HTTP::Post.new uri.request_uri
+        req.set_form_data data
+      else
+        req = Net::HTTP::Get.new uri.request_uri
+      end
+
       req['PRIVATE-TOKEN'] = token
-
-      http = Net::HTTP.new uri.host, uri.port
-      res = http.request req
-
-      JSON.parse res.body
-    end
-
-    def do_post(url, data)
-
-      uri = URI "#{gitlab_url}/#{url}"
-      req = Net::HTTP::Post.new uri.request_uri
-      req['PRIVATE-TOKEN'] = token
-      req.set_form_data data
 
       http = Net::HTTP.new uri.host, uri.port
       res = http.request req
