50102cedd102ee9d9bb58e18ca9b793d2a10dc77
Javier Palacios <javiplx@gmail.com>
2015-02-23 21:42:15 +0100

    Add rspec tests for tag aware parameter substitution

--- spec/services/get_parameters_values_spec.rb
+++ spec/services/get_parameters_values_spec.rb
@@ -40,12 +40,33 @@ module GitlabWebHook
     end
 
     context 'with branch parameter' do
-      it 'replaces it with data from details' do
-        branch_parameter = build_parameter('commit_branch_parameter', 'default_branch')
+
+      let(:tag_payload) { JSON.parse(File.read('spec/fixtures/default_tag.json')) }
+      let(:tag_details) { PayloadRequestDetails.new(tag_payload) }
+
+      before :each do
         allow(project).to receive(:get_branch_name_parameter) { branch_parameter }
         allow(project).to receive(:get_default_parameters) { [branch_parameter] }
-        allow(details).to receive(:branch) { 'commit_branch' }
-        expect(subject.with(project, details)[0].value).to eq('commit_branch')
+      end
+
+      context 'with paramter for branch' do
+        let(:branch_parameter) { build_parameter('commit_branch_parameter', 'default_branch') }
+        it 'replaces it under push event' do
+          expect(subject.with(project, details)[0].value).to eq('master')
+        end
+        it 'uses default under tag event' do
+          expect(subject.with(project, tag_details)[0].value).to eq('default_branch')
+        end
+      end
+
+      context 'with parameter for tag' do
+        let(:branch_parameter) { build_parameter('tagname', '-') }
+        it 'uses default under push event' do
+          expect(subject.with(project, details)[0].value).to eq('-')
+        end
+        it 'replaces it under tag event' do
+          expect(subject.with(project, tag_details)[0].value).to eq('v1.0.0')
+        end
       end
     end
 
